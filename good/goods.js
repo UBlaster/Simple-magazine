73b71ddedec1261311c1688df13f4344d7fdf36a
Dmitriy <tarusovtdv@gmail.com>
2023-03-10 22:57:15 +0500

    simple store

---
+++ good/goods.js
@@ -0,0 +1,17 @@
+const GOODS = [{
+    count:0,
+    id: 1,
+    name: 'Уроки японского Лайт',
+    price: 999,
+    imgSrc: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/1200px-Flag_of_Japan.svg.png',
+},{
+    id: 2,
+    name: 'Уроки японского Мидл',
+    price: 9999,
+    imgSrc: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/1200px-Flag_of_Japan.svg.png',
+},{
+    id: 3,
+    name: 'Уроки японского Хард',
+    price: 99999,
+    imgSrc: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/1200px-Flag_of_Japan.svg.png',
+}];
